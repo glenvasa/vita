0a83d062d17e77b47e49786862fd63d113aa009b
glenvasa <70590043+glenvasa@users.noreply.github.com>
2020-12-05 20:43:18 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,21 @@ const app = express();
 const connectToDatabase = require("./config/connectToDatabase");
 const cors = require("cors");
 
-app.use(cors());
-
-app.use(express.json({ extended: false }));
-
+// Function that connects express app to database
 connectToDatabase();
 
-app.get("/", (req, res) => res.send("App is working! Congratulations"));
+// used to prevent cors policy warning
+app.use(cors());
 
+// allows us to use req.body in posts
+app.use(express.json({ extended: false }));
+
+// Routes
+app.use("/api/posts", require("./routes/users.js"));
+app.use("/api/users", require("./routes/users.js"));
+
+// create variable to specify which port app will run; depends if runnng on heroku or locally
 let PORT = process.env.PORT || 5000;
 
+// metod to specify on which port we want our app to run w/callback function to check if working
 app.listen(PORT, () => console.log(`Server is on port: ${PORT}`));
