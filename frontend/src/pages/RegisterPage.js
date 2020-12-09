636da3bf625a0e071631e99aeba07402d4f304b4
glenvasa <70590043+glenvasa@users.noreply.github.com>
2020-12-09 08:36:56 -0500

    Created input fields and functionality

--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { registerUser } from "../actions/auth.actions";
+import { connect } from "react-redux";
 
-const RegisterPage = () => {
+const RegisterPage = ({ registerUser }) => {
+  const [userData, setUserData] = useState({
+    name: "",
+    lastName: "",
+    userName: "",
+    email: "",
+    password: "",
+  });
+
+  const { name, lastName, userName, email, password } = userData;
+
+  const onChange = (e) =>
+    setUserData({ ...userData, [e.target.name]: e.target.value });
+
   return (
     <div>
       <div>
+        <label>First Name</label>
         <input
           type="text"
           name="name"
@@ -11,8 +27,50 @@ const RegisterPage = () => {
           onChange={(e) => onChange(e)}
         />
       </div>
+      <br />
+      <div>
+        <label>Last Name</label>
+        <input
+          type="text"
+          name="lastName"
+          value={lastName}
+          onChange={(e) => onChange(e)}
+        />
+      </div>
+      <br />
+      <div>
+        <label>Username</label>
+        <input
+          type="text"
+          name="userName"
+          value={userName}
+          onChange={(e) => onChange(e)}
+        />
+      </div>
+      <br />
+      <div>
+        <label>Email</label>
+        <input
+          type="text"
+          name="email"
+          value={email}
+          onChange={(e) => onChange(e)}
+        />
+      </div>
+      <br />
+      <div>
+        <label>Password</label>
+        <input
+          type="text"
+          name="password"
+          value={password}
+          onChange={(e) => onChange(e)}
+        />
+      </div>
+      <br />
+      <button onClick={() => registerUser(userData)}>Submit Form</button>
     </div>
   );
 };
 
-export default RegisterPage;
+export default connect(null, { registerUser })(RegisterPage);
