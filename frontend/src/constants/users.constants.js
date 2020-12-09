72ee4e2582f21a65feeb2d0f79bff70825f945ee
glenvasa <70590043+glenvasa@users.noreply.github.com>
2020-12-09 15:49:56 -0500

    created users.constants.js

---
+++ frontend/src/constants/users.constants.js
@@ -0,0 +1,9 @@
+export const GET_USERS = "GET_USERS";
+export const GET_USER = "GET_USER";
+export const SEARCH_USERS = "SEARCH_USERS";
+export const CHANGE_EMAIL = "CHANGE_EMAIL";
+export const CHANGE_USERNAME = "CHANGE_USERNAME";
+export const CHANGE_LASTNAME = "CHANGE_LASTNAME";
+export const CHANGE_NAME = "CHANGE_NAME";
+export const CHECK_PASSWORDS = "CHECK_PASSWORDS";
+export const CHANGE_PASSWORD = "CHANGE_PASSWORD";
