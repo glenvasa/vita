a5cd57c1d7001910cdc732f3e423af069a1f4ebe
glenvasa <70590043+glenvasa@users.noreply.github.com>
2020-12-09 20:07:46 -0500

    Moved 6 constants to auth.constants and added 3 from posts.constants

--- frontend/src/constants/users.constants.js
+++ frontend/src/constants/users.constants.js
@@ -1,9 +1,8 @@
 export const GET_USERS = "GET_USERS";
 export const GET_USER = "GET_USER";
 export const SEARCH_USERS = "SEARCH_USERS";
-export const CHANGE_EMAIL = "CHANGE_EMAIL";
-export const CHANGE_USERNAME = "CHANGE_USERNAME";
-export const CHANGE_LASTNAME = "CHANGE_LASTNAME";
-export const CHANGE_NAME = "CHANGE_NAME";
-export const CHECK_PASSWORDS = "CHECK_PASSWORDS";
-export const CHANGE_PASSWORD = "CHANGE_PASSWORD";
+export const USER_ERROR = "USER_ERROR";
+export const GET_USER_POSTS = "GET_USER_POSTS";
+export const GET_POST_BY_USER_ID = "GET_POST_BY_USER_ID";
+export const GET_USER_BY_ID = "GET_USER_BY_ID";
+export const SEARCH_BY_USERNAME = "SEARCH_BY_USERNAME";
